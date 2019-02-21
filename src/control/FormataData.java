ffbdb9fd1d8bd5d20c368b2cc1648a7874b1ee3f
felip <felip@ASUS.local>
2019-02-20 23:27:33 -0300

    formata data

---
+++ src/control/FormataData.java
@@ -0,0 +1,42 @@
+package control;
+
+public class FormataData {
+
+	String datafinal;
+	public String getDatafinal() {
+		return datafinal;
+	}
+	public void setDatafinal(String datafinal) {
+		this.datafinal = datafinal;
+	}
+	public void Formatar(String i,int opera) {
+		
+		String dia,mes,ano,dt[];
+		if(opera==1) {
+			
+			if(i.equals("")) {
+				dt=i.split("/");
+				setDatafinal("");
+			}else {
+				dt=i.split("/");
+				dia = dt[0];
+				mes=dt[1];
+				ano=dt[2];
+				setDatafinal(ano+"-"+mes+"-"+dia);
+			}
+			
+		}else {
+			if(i!=null) {
+			dt=i.split("-");
+			ano=dt[0];
+			mes=dt[1];
+			dia = dt[2];
+			setDatafinal(dia+mes+ano);
+			}else {
+				setDatafinal("");
+			}
+		}
+		
+		
+	}
+}
