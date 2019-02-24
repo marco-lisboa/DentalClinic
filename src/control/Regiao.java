bd3992c8c9f618593d0b2a42c289ab8c9a8a54f2
felip <felip@ASUS.local>
2019-02-23 23:13:35 -0300

    regiao

---
+++ src/control/Regiao.java
@@ -0,0 +1,56 @@
+package control;
+
+public class Regiao {
+
+	String nomePais,siglaPAis,nomeEstado,ufEstado,nomeCidade;
+	public String getNomePais() {
+		return nomePais;
+	}
+	public void setNomePais(String nomePais) {
+		this.nomePais = nomePais;
+	}
+	public String getSiglaPAis() {
+		return siglaPAis;
+	}
+	public void setSiglaPAis(String siglaPAis) {
+		this.siglaPAis = siglaPAis;
+	}
+	public String getNomeEstado() {
+		return nomeEstado;
+	}
+	public void setNomeEstado(String nomeEstado) {
+		this.nomeEstado = nomeEstado;
+	}
+	public String getUfEstado() {
+		return ufEstado;
+	}
+	public void setUfEstado(String ufEstado) {
+		this.ufEstado = ufEstado;
+	}
+	public String getNomeCidade() {
+		return nomeCidade;
+	}
+	public void setNomeCidade(String nomeCidade) {
+		this.nomeCidade = nomeCidade;
+	}
+	public int getIdPais() {
+		return idPais;
+	}
+	public void setIdPais(int idPais) {
+		this.idPais = idPais;
+	}
+	public int getIdEstado() {
+		return idEstado;
+	}
+	public void setIdEstado(int idEstado) {
+		this.idEstado = idEstado;
+	}
+	public int getIdCidade() {
+		return idCidade;
+	}
+	public void setIdCidade(int idCidade) {
+		this.idCidade = idCidade;
+	}
+	int idPais,idEstado,idCidade;
+	
+}
