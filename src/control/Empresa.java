4b03fe8ac27fdad2ed8286e2ec21311758478b7f
sedsoft <sedsoft@sed>
2019-04-04 12:32:03 -0300

    control empresa

---
+++ src/control/Empresa.java
@@ -0,0 +1,183 @@
+package control;
+
+public class Empresa {
+	String nomeFantasia, razaoSocial, cnpj, estado, cidade, rua, cep, numero,contato1,contato2,op1,op2, email1,email2,
+	proprietario,cpf,cro,emailProprietario,contato1Pro,contato2Pro,op1Pro,op2Pro;
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getContato1() {
+		return contato1;
+	}
+
+	public void setContato1(String contato1) {
+		this.contato1 = contato1;
+	}
+
+	public String getContato2() {
+		return contato2;
+	}
+
+	public void setContato2(String contato2) {
+		this.contato2 = contato2;
+	}
+
+	public String getOp1() {
+		return op1;
+	}
+
+	public void setOp1(String op1) {
+		this.op1 = op1;
+	}
+
+	public String getOp2() {
+		return op2;
+	}
+
+	public void setOp2(String op2) {
+		this.op2 = op2;
+	}
+
+	public String getEmail1() {
+		return email1;
+	}
+
+	public void setEmail1(String email1) {
+		this.email1 = email1;
+	}
+
+	public String getEmail2() {
+		return email2;
+	}
+
+	public void setEmail2(String email2) {
+		this.email2 = email2;
+	}
+
+	public String getProprietario() {
+		return proprietario;
+	}
+
+	public void setProprietario(String proprietario) {
+		this.proprietario = proprietario;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCro() {
+		return cro;
+	}
+
+	public void setCro(String cro) {
+		this.cro = cro;
+	}
+
+	public String getEmailProprietario() {
+		return emailProprietario;
+	}
+
+	public void setEmailProprietario(String emailProprietario) {
+		this.emailProprietario = emailProprietario;
+	}
+
+	public String getContato1Pro() {
+		return contato1Pro;
+	}
+
+	public void setContato1Pro(String contato1Pro) {
+		this.contato1Pro = contato1Pro;
+	}
+
+	public String getContato2Pro() {
+		return contato2Pro;
+	}
+
+	public void setContato2Pro(String contato2Pro) {
+		this.contato2Pro = contato2Pro;
+	}
+
+	public String getOp1Pro() {
+		return op1Pro;
+	}
+
+	public void setOp1Pro(String op1Pro) {
+		this.op1Pro = op1Pro;
+	}
+
+	public String getOp2Pro() {
+		return op2Pro;
+	}
+
+	public void setOp2Pro(String op2Pro) {
+		this.op2Pro = op2Pro;
+	}
+
+}
